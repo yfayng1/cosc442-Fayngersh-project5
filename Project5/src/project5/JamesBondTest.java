f7650a8462fc1e126874ea4c77563ab2b198498a
Fima Fayngersh <yfayng1@students.towson.edu>
2016-04-22 23:44:55 -0400

    Task3
    Started working on it, but didn't finish. Added a directory Task3. Added
    a Project5 with JamesBond.java and JamesBondTest.java.

---
+++ Project5/src/project5/JamesBondTest.java
@@ -0,0 +1,14 @@
+package project5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JamesBondTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
